f7a769eca5b76c2cf7126f7a555b6137a0019a44
bax <dev.lasota@gmail.com>
2022-06-07 15:53:13 +0200

    change names of function parameters, modify localStorage, change let to const,

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # shopping-card
 Simple shopping cart project using vanilla JavaScript. 
 Based on freeCodeCamp.org
-https://wojciech-lasota.github.io/clothing-store
+https://wojciech-lasota.github.io/shopping-card/
--- main.js
+++ main.js
@@ -1,47 +1,45 @@
-const shop = document.getElementById('shop');
+const shop = document.getElementById("shop");
 //console.log(shop);
 const basket = JSON.parse(localStorage.getItem("data")) || [];
 console.log(basket);
 
-const shopItemsData = [{
+const inventoryList = [
+  {
     id: "1",
     name: "Casual Shirt",
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-1.jpg"
-},
-{
+    img: "images/img-1.jpg",
+  },
+  {
     id: "2",
     name: "Office Shirt",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-2.jpg"
-},
-{
+    img: "images/img-2.jpg",
+  },
+  {
     id: "3",
     name: "T Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-3.jpg"
-},
-{
+    img: "images/img-3.jpg",
+  },
+  {
     id: "4",
     name: "Mens Suit",
     price: 300,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-4.jpg"
-}];
-
-
+    img: "images/img-4.jpg",
+  },
+];
 
 const generateShop = () => {
-    return (shop.innerHTML = shopItemsData
-      .map((x) => {
-        const {id, name, price, desc, img } = x;
-        const search = basket.find((x)=>x.id == id) || [];
-        console.log(basket[0].id)
-        console.log(search);
-        return `
+  return (shop.innerHTML = inventoryList
+    .map(({ id, name, price, desc, img }) => {
+      const search = basket.find(({ id: basketId }) => id == basketId) || [];
+      console.log(search);
+      return `
         <div id=product-id-${id} class="item">
                 <img width ="220" src="${img}" alt="">
                 <div class="details display-f fd-c">
@@ -52,61 +50,64 @@ const generateShop = () => {
                         <div class="buttons display-f">
                             <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                             <div id=${id} class="quantity">
-                            ${search.item === undefined ? 0 : search.item}
+                            ${search.amount === undefined ? 0 : search.amount}
                             </div>
                             <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                         </div>
                     </div>
                 </div>
             </div>
-        `
-    }).join(""))
+        `;
+    })
+    .join(""));
 };
 
 generateShop();
 
-const increment = (id) =>{
-    const selectedItem = id;
-    // console.log(selectedItem);
+const increment = (id) => {
+  //   const selectedItem = id;
+  // console.log(selectedItem);
 
-    const search = basket.find((x)=> x.id === id)
-    if(search === undefined){
-        basket.push({
-            id:id,
-            item:1,
-    
-        });
-    }else{
-        search.item++;
-    }
+  const search = basket.find((x) => x.id === id);
+  if (search === undefined) {
+    basket.push({
+      id,
+      amount: 1,
+    });
+  } else {
+    search.amount++;
+  }
 
-    localStorage.setItem("data",JSON.stringify(basket));
-    // console.log(basket);
-    update(id);
-    
+  localStorage.setItem("data", JSON.stringify(basket));
+  // console.log(basket);
+  update(id);
 };
-const decrement = (id) =>{
-    
+const decrement = (id) => {
+  const search = basket.find((x) => x.id === id);
+  if (search.amount === 0) {
+    return;
+  } else {
+    search.amount--;
+  }
 
-    const search = basket.find((x)=> x.id === id)
-    if(search.item === 0){
-        return;
-    }else{
-        search.item--;
-    }
-
-    localStorage.setItem("data", JSON.stringify(basket));
-    // console.log(basket);
-    update(id);
-    
+  localStorage.setItem("data", JSON.stringify(basket));
+  // console.log(basket);
+  update(id);
 };
-const update = (id) =>{
-    const search = basket.find((x) => x.id === id);
-    document.getElementById(id).innerHTML = search.item;
-    calculation();
+const updateCartAmount = () => {
+  const cartAmount = document.getElementById("cartAmount");
+  cartAmount.innerHTML = basket.reduce(
+    (itemsAmount, { amount }) => itemsAmount + amount,
+    0
+  );
+};
+const update = (id) => {
+  const search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.amount;
+  calculation();
 };
 
 const calculation = () => {
-    const cartIcon = document.getElementById("cartAmount");
-    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
-  };
+  const cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.amount).reduce((x, y) => x + y, 0);
+};
