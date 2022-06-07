677e9e1e26d9f96bb027fbb7774e8ca1df18164b
bax <dev.lasota@gmail.com>
2022-06-07 12:31:13 +0200

    inc/decrement

---
+++ main.js
@@ -0,0 +1,89 @@
+let shop = document.getElementById('shop');
+
+let shopItemsData = [{
+    id: "1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg"
+},
+{
+    id: "2",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg"
+},
+{
+    id: "3",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg"
+},
+{
+    id: "4",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg"
+}];
+
+let basket = [];
+
+let generateShop = () => {
+    return (shop.innerHTML = shopItemsData.map((x) => {
+        let { id, name, price, desc, img } = x
+        return `
+        <div id=product-id-${id} class="item">
+                <img width ="220" src="${img}" alt="">
+                <div class="details display-f fd-c">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price-quantity display-f jc-sb ai-c">
+                        <h2>$ ${price}</h2>
+                        <div class="buttons display-f">
+                            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                            <div id=${id} class="quantity">0</div>
+                            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }).join(""))
+};
+
+generateShop();
+
+let increment = (id) =>{
+    console.log(basket);
+
+    let search = basket.find((x)=> x.id === id)
+    if(search === undefined){
+        basket.push({
+            id:id,
+            item:1,
+    
+        });
+    }else{
+        search.item++;
+    }
+
+    
+    console.log(basket);
+};
+let decrement = (id) =>{
+    console.log(basket);
+
+    let search = basket.find((x)=> x.id === id)
+    if(search.item === 0){
+        return;
+    }else{
+        search.item--;
+    }
+
+    
+    console.log(basket);
+};
+let update = () =>{};
